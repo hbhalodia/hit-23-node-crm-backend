96df1db73a63a934f9a0fd04285f8007345e7fb4
Hit Bhalodia <58802366+hbhalodia@users.noreply.github.com>
2020-07-19 12:04:23 +0530

    Create student-schema.js

---
+++ models/student-schema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const studentSchema = mongoose.Schema({
+    name:{type:String , required:true},
+    branch:{type:String , required:true},
+    email:{type:String, required:true},
+    creator :{ type : mongoose.Schema.Types.ObjectId, ref:"User", required:true }
+})
+
+module.exports = mongoose.model('Student',studentSchema)
